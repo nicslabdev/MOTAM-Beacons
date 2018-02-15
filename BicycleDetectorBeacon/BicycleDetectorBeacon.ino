4fb4ab262d145583c092de34d1e27e3c211f53cf
Manuel Montenegro <manumonti@gmail.com>
2018-02-15 19:57:41 +0100

    Changed GPS coordenates

--- BicycleDetectorBeacon/BicycleDetectorBeacon.ino
+++ BicycleDetectorBeacon/BicycleDetectorBeacon.ino
@@ -19,16 +19,16 @@ uint8_t advdata[] =
   0xDE,  
   
   // Latitude of the TrafficLightDetectorBeacon, in LE float format (not present)
-  0xFF,
-  0xFF,
-  0xFF,
-  0xFF,
+  0xEC,
+  0xDB,
+  0x12,
+  0x42,
   
   // Longitude of the TrafficLightDetectorBeacon, in LE float format (not present)
-  0xFF,
-  0xFF,
-  0xFF,
-  0xFF,
+  0x9D,
+  0xE4,
+  0x8F,
+  0xC0,
   
   0x03,  // Type of DEphisit BEacon (0x01 -> Traffic Sign Beacon, 0x02 -> Weather Beacon, 0x03 -> Bicycle Beacon)
   
